afde28a8e8ed4d8bf1b0b8ba4160fa2bef24c67c
Kooda <kooda@upyum.com>
2015-01-24 11:22:02 +0100

    editor and message receipt

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,6 @@
 local socket = require("socket")
 
+cursor = {1, 1}
 
 -- Modules loading
 modules = {}
@@ -14,58 +15,14 @@ local function modules_load()
    end
 end
 
-
--- Love callbacks
-
-function love.textinput(text)
-end
-
-function love.draw()
+local function modules_call(which, ...)
    for _, module in pairs(modules) do
-      if module.draw then module.draw() end
-   end
-
-   for l, line in ipairs(s) do
-      for c, spec in ipairs(line) do
-         local char, r, v, b = unpack(spec)
-         love.graphics.setColor(r, v, b)
-         love.graphics.print(char, c * 10, l * 10)
-      end
+      local method = module[which]
+      if method then method(...) end
    end
 end
 
-function love.update(dt)
-   for _, module in pairs(modules) do
-      if module.update then module.update(dt) end
-   end
-
-   local msg, err = server:receive("*l")
-   if msg then
-      table.insert(msgs, msg)
-   end
-end
-
-function love.keypressed(key)
-   for _, module in pairs(modules) do
-      if module.keypressed then module.keypressed(key) end
-   end
-end
-
-function love.load(args)
-   for k,v in pairs(args) do
-      print(k,v)
-   end
-
-   server = socket.connect(args[3], tonumber(args[4]))
-   if not server then
-      print("failed to connect to server")
-      love.event.quit()
-   end
-   init_screen()
-   server:settimeout(0)
-end
-
-function init_screen()
+local function init_screen()
    local msg, err = server:receive("*l")
    local nlines = string.match(msg, "dump (%d+)")
 
@@ -79,6 +36,126 @@ function init_screen()
          table.insert(s[i], {char, tonumber(r), tonumber(v), tonumber(b)})
       end
    end
+end
+
+local parsers = {}
+
+function parsers.move(msg)
+   local x, y = msg:match("^move (%d+) (%d+)$")
+   if x then
+      cursor = {x, y}
+      return true
+   end
+end
+
+function parsers.ins(msg)
+   local x, y, letter, r, v, b = msg:match("^ins (%d+) (%d+) (.+) (%d+) (%d+) (%d+)$")
+   if x then
+      ins_char(x, y, {letter, r, v, b})
+   end
+end
+
+local function handle_client_msg(msg)
+   for _, parser in pairs(parsers) do
+      if parser(msg) then return end
+   end
+end
+
+-- Love callbacks
+
+-- boilerplate code
+function love.load(args)
+   local font = love.graphics.newFont("terminus.ttf", 20)
+   love.graphics.setFont(font)
+
+   fontwidth = font:getWidth("m")
+   fontheight = font:getHeight("m")
+
+   for k,v in pairs(args) do
+      print(k,v)
+   end
+
+   server = socket.connect(args[3], tonumber(args[4]))
+   if not server then
+      print("failed to connect to server")
+      love.event.quit()
+   end
+   init_screen()
+   server:settimeout(0)
+
+   modules_call("load", args)
+end
+
+function love.draw()
+   -- background
+   --love.graphics.setColor(255, 255, 255)
+   --love.graphics.rectangle("fill", 0, 0, 800, 600)
+
+   -- border
+   --love.graphics.setColor(180, 180, 180)
+   --love.graphics.setLineWidth(1)
+   --love.graphics.rectangle("line", fontwidth, fontheight, 70 * fontwidth, 25 * fontheight)
+
+   -- cursor
+   love.graphics.setColor(180, 180, 180)
+   love.graphics.rectangle("fill", cursor[1] * fontwidth, cursor[2] * fontheight, fontwidth, fontheight)
+
+   -- text buffer
+   for l, line in ipairs(s) do
+      for c, spec in ipairs(line) do
+         local char, r, v, b = unpack(spec)
+         love.graphics.setColor(r, v, b)
+         love.graphics.print(char, c * fontwidth, l * fontheight)
+      end
+   end
+
+   modules_call("draw") -- XXX should modules hide text? if not put that at the top of love.draw
+end
+
+
+-- events handling
+
+function love.update(dt)
+   local msg, err = server:receive("*l")
+   if not msg then
+      if err == "closed" then
+         love.event.quit()
+      end
+   else
+      handle_client_msg(msg)
+   end
+
+   modules_call("update", dt)
+end
+
+function love.textinput(text)
+   server:send("char " .. text)
+
+   modules_call("textinput", text)
+end
+
+function love.keypressed(key)
+   if key == "left"
+      or key == "right"
+      or key == "up"
+      or key == "down"
+      then
+         server:send("dir " .. key)
+   elseif key == "backspace" or key == "enter" then
+      server:send(key)
+   end
+
+   modules_call("keypressed", key)
+end
+
+function love.mousepressed(x, y, button)
+   if button == "l" then
+      local c, l = math.floor(x / fontwidth), math.floor(y / fontheight)
+      print(c, l)
+      server:send("pos " .. c .. " " .. l)
+   end
+
+   modules_call("mousepressed", x, y, button)
 end
 
 modules_load()
