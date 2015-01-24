62b0777903b46e053d69816b4484fcdbd108f4db
Kooda <kooda@upyum.com>
2015-01-25 00:15:17 +0100

    move cursor/border effect

--- achievements/ach.lua
+++ achievements/ach.lua
@@ -1,3 +1,5 @@
+local clock = 0
+local draw_cursor = false
 
 local m = {}
 
@@ -6,8 +8,40 @@ function m.ach()
    ach('ach')
 end
 
+function m.load()
+   m.activated = true
+end
+
 function m.activate(box)
-  if box then box_push('First achievement!') end
+   m.activated = false
+   if box then box_push('First achievement!') end
+   music.playloop(1)
+end
+
+function m.update(dt)
+   if m.activated then return end
+
+   clock = clock + dt
+   if clock >= 0.5 then
+      draw_cursor = not draw_cursor
+      clock = 0
+   end
+end
+
+function m.pre_draw()
+   if m.activated then return end
+
+   -- cursor
+   if draw_cursor then
+      love.graphics.setColor(180, 180, 180)
+      love.graphics.rectangle("fill", cursor[1] * fontwidth, cursor[2] * fontheight, fontwidth, fontheight)
+   end
+
+   -- border
+   love.graphics.setColor(220, 220, 220)
+   love.graphics.setLineStyle("rough")
+   love.graphics.setLineWidth(1)
+   love.graphics.rectangle("line", fontwidth, fontheight, cols * fontwidth, #s * fontheight)
 end
 
 return m
--- achievements/firstchar.lua
+++ achievements/firstchar.lua
@@ -1,49 +1,12 @@
-local clock = 0
-local draw_cursor = false
-
 local m = {}
-
---- XXX désactiver l'affichage du curseur et de la bordure avant l'activation de l'achievement
 
 function m.char()
    m.activated = true
    ach('firstchar')
 end
 
-function m.load()
-   m.activated = false
-end
-
 function m.activate(box)
-   m.activated = false
    if box then box_push('You first character!') end
-   music.playloop(1)
-end
-
-function m.update(dt)
-   if m.activated then return end
-
-   clock = clock + dt
-   if clock >= 0.5 then
-      draw_cursor = not draw_cursor
-      clock = 0
-   end
-end
-
-function m.pre_draw()
-   if m.activated then return end
-
-   -- cursor
-   if draw_cursor then
-      love.graphics.setColor(180, 180, 180)
-      love.graphics.rectangle("fill", cursor[1] * fontwidth, cursor[2] * fontheight, fontwidth, fontheight)
-   end
-
-   -- border
-   love.graphics.setColor(220, 220, 220)
-   love.graphics.setLineStyle("rough")
-   love.graphics.setLineWidth(1)
-   love.graphics.rectangle("line", fontwidth, fontheight, cols * fontwidth, #s * fontheight)
 end
 
 return m
