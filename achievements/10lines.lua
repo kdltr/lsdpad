98b00ae27c8e3ddf27b7baf1e8e4628134990c60
Kooda <kooda@upyum.com>
2015-01-24 21:38:08 +0100

    10lines

---
+++ achievements/10lines.lua
@@ -0,0 +1,18 @@
+local m = {}
+
+local fun = function()
+   print("==== " .. #s)
+   if #s >= 20 then
+      ach("10lines")
+      m.activated = true
+   end
+end
+
+m.newline = fun
+m.char = fun
+
+function m.activate(box)
+   if box then box_push "Long file is long" end
+end
+
+return m
