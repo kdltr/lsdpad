99d2be68e15962c2070c8ea9e7dccd45542860eb
Jobastre <jfarnaud@selen.so>
2015-01-24 18:39:04 +0100

    better algorithm

--- achievements/10samechar.lua
+++ achievements/10samechar.lua
@@ -3,34 +3,26 @@ local activated = false
 local m = {}
 
 function m.init_client(client)
-   client.last_chars = {'', '', '', '', '', '', '', '', '', ''}
+   client.last_char = ''
+   client.last_char_count = 0
 end
 
 function m.char(client, char)
    if activated then return end
-   table.insert(client.last_chars, char)
-   table.remove(client.last_chars, 1)
-   print(table.concat(client.last_chars))
-   local c = false
-   local all_same = true
-   for _, v in ipairs(client.last_chars) do
-      if not c then
-         c = v
-      else
-         if c ~= v then
-            all_same = false
-            break
-         end
+   if char == client.last_char then
+      client.last_char_count = client.last_char_count + 1
+      if client.last_char_count == 10 then
+         activated = true
+         ach(client.s, '10samechar')
       end
-   end
-   if all_same then
-      activated = true
-      ach(client.s, '10samechar')
+   else
+      client.last_char_count = 0
+      client.last_char = char
    end
 end
 
-function m.activate()
-   box_push('10 times the same character!')
+function m.activate(box)
+  if box then  box_push('10 times the same character!') end
 end
 
 return m
